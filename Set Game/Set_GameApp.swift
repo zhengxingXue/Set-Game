5f10afff3ef1e6daf3705a2f1ba445062c6670c3
zhengxingXue <zx252cornell@gmail.com>
2021-05-05 23:17:08 -0500

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Jim's MacBook Pro on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
